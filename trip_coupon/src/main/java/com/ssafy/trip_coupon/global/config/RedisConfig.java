8f26c0d780a7019b3a8a05187c4c89964433d8a0
chj <chjcoder@gmail.com>
2024-11-02 00:12:03 +0900

    feat(login) : redis 연결 풀 설정 추가

--- trip_coupon/src/main/java/com/ssafy/trip_coupon/global/config/RedisConfig.java
+++ trip_coupon/src/main/java/com/ssafy/trip_coupon/global/config/RedisConfig.java
@@ -1,9 +1,12 @@
 package com.ssafy.trip_coupon.global.config;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
@@ -11,7 +14,14 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(); // Lettuce 클라이언트 사용
+        GenericObjectPoolConfig<Object> poolConfig = new GenericObjectPoolConfig<>();
+        poolConfig.setMaxTotal(10); // 최대 연결 수
+        poolConfig.setMaxIdle(5); // 유휴 상태의 최대 연결 수
+        poolConfig.setMinIdle(1); // 유휴 상태의 최소 연결 수
+
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(), LettucePoolingClientConfiguration.builder()
+                .poolConfig(poolConfig)
+                .build());
     }
 
     @Bean
